5817636de70d64a34f9455dccc095416cba7d1bb
dominik-lenovo <dominwro@gmail.com>
2019-10-26 11:50:42 +0200

    Initial commit.

--- src/main/java/spring/mvc/config/ApplicationInitializer.java
+++ src/main/java/spring/mvc/config/ApplicationInitializer.java
@@ -1,13 +1,27 @@
 package spring.mvc.config;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 public class ApplicationInitializer implements WebApplicationInitializer {
     @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
+    public void onStartup(ServletContext servletContext) throws ServletException
+    {
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        rootContext.register(RootConfig.class);
+        servletContext.addListener(new ContextLoaderListener(rootContext));
 
+        AnnotationConfigWebApplicationContext webContext = new AnnotationConfigWebApplicationContext();
+        webContext.register(WebConfig.class);
+
+        ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcherServlet", new DispatcherServlet(webContext));
+        registration.addMapping("/");
+        registration.setLoadOnStartup(1);
     }
 }
